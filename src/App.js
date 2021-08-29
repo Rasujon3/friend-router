f29472c81e103bd4887a2e5924de96f4362da594
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-08-29 13:36:41 +0600

    dynamic router setup
    video : 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 // import logo from './logo.svg';
-import { useEffect, useState } from 'react';
 import './App.css';
 import {
   BrowserRouter as Router,
@@ -7,22 +6,29 @@ import {
   Route,
   Link
 } from "react-router-dom";
-import Friend from './components/Friend/Friend';
+import Home from './components/Home/Home';
+import NoMatch from './NoMatch/NoMatch';
 
 function App() {
-  const [friends, setFriends] = useState([]);
-  useEffect(()=>{
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(res=>res.json())
-    .then(data=>setFriends(data))
-  },[])
+  
   return (
-    <div>
-      <h1>Friends : {friends.length}</h1>
-      {
-        friends.map(friend => <Friend friend={friend}></Friend> )
-      }
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/home">
+            <Home/>
+        </Route>
+
+        <Route exact path="/">
+            <Home />
+          </Route>
+
+        
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        
+      </Switch>
+    </Router>
   );
 }
 
